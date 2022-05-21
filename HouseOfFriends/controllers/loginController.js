88e0954f9f60a3543ce638793f6879e7e39185a3
Sristi27-sys <sristirajput27@gmail.com>
2022-05-21 23:14:01 +0530

    fixes

--- HouseOfFriends/controllers/loginController.js
+++
@@ -1,5 +0,0 @@
-const Login = require('../models/loginSchema')
-
-exports.getLoginCredentials = ( req, res) => {
-    res.status(200).json({message:" route is working fine"})
-}
--- HouseOfFriends/routes/loginRoute.js
+++
@@ -1,8 +0,0 @@
-const express = require('express')
-const { getLoginCredentials } = require("../controllers/loginController")
-
-const router = express.Router();
-
-router.route("/login").get(getLoginCredentials)
-
-module.exports = router
